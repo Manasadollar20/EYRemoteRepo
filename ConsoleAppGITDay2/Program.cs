2d8360912be33d705d0ebc27b6383edde2832c38
Manasadollar20 <manasanettimi2005@gmail.com>
2024-04-29 16:18:56 +0530

    added new line local changes

--- ConsoleAppGITDay2/Program.cs
+++ ConsoleAppGITDay2/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("manu");
             Console.Writeline("hola");
+            Console.WriteLine("local changes");
         }
     }
 }
